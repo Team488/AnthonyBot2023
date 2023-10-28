c0da08fc616298821146471a75f46553fdbe405c
anthonytrenh <trenhanthony@gmail.com>
2023-10-28 11:37:29 -0700

    Sat 10/28
    timestamp for before i try to reorganize my code

--- src/main/java/competition/subsystems/SubsystemDefaultCommandMap.java
+++ src/main/java/competition/subsystems/SubsystemDefaultCommandMap.java
@@ -3,7 +3,9 @@ package competition.subsystems;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import competition.subsystems.drive.DriveSubsystem;
 import competition.subsystems.drive.commands.SteerAndDriveJoystickCommand;
+import competition.subsystems.drive.commands.SwerveDriveWithJoysticksCommand;
 import competition.subsystems.drive.swerve.SwerveSteerSubsystem;
 
 /**
@@ -24,4 +26,9 @@ public class SubsystemDefaultCommandMap {
     public void setupSwerveSteeringSubsystem(SwerveSteerSubsystem swerveSteerSubsystem, SteerAndDriveJoystickCommand steerAndDriveJoystickCommand) {
         swerveSteerSubsystem.setDefaultCommand(steerAndDriveJoystickCommand);
     }
+
+    // @Inject
+    // public void setupDriveSubsystem(DriveSubsystem driveSubsystem, SwerveDriveWithJoysticksCommand command) {
+    //     driveSubsystem.setDefaultCommand(command);
+    // }
 }
